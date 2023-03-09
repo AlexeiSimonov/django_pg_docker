511b88a38c34d4190f0e6767a60be41229e2a275
AlexeiSimonov <sushka2820655@yandex.ru>
2023-03-09 17:36:56 +0300

    add docker compose

--- django_pg_docker/settings.py
+++ django_pg_docker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,7 +81,7 @@ DATABASES = {
         'NAME': 'Example',
         'USER': 'alexei',
         'PASSWORD': 'ge3038206',
-        'HOST': 'localhost',
+        'HOST': f'{os.getenv("POSTGRES_HOST")}',
         'PORT': '5432',
     }
 }
